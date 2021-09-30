6c999667cf1ca7c2f73333268502629b6abc216a
Vladimir Stojanovic <st.vladas@hotmail.com>
2021-09-30 21:56:20 +0200

    assets dir made, vanilla JS form validation

--- main.js
+++ main.js
@@ -1,90 +1,79 @@
-//validate FirstName
-function validateFirstName() {
-	const firstName = $('#fname').val();
-	const $firstNameInput = $('#fname');
-	const $fNameErrorIcon = $('#fname-error-icon');
-	const $fNameErrorText = $('#fname-error-text');
+class FormValidator {
+	constructor(form, fields) {
+		this.form = form
+		this.fields = fields
+	}
 
-	if (firstName === '' || firstName == null) {
-		$fNameErrorIcon.css({ display: 'block' });
-		$fNameErrorText.css({ display: 'block' });
-		$firstNameInput.css({ border: '2px solid #ff7a7a' });
-	} else {
-		$fNameErrorIcon.css({ display: 'none' });
-		$fNameErrorText.css({ display: 'none' });
-		$firstNameInput.css({ border: '1px solid #dedede' });
+	initialize() {
+		this.validateOnEntry()
+		this.validateOnSubmit()
+	}
+
+	validateOnSubmit() {
+		let self = this
+
+		this.form.addEventListener('submit', (e) => {
+			e.preventDefault()
+			self.fields.forEach((field) => {
+				const input = document.querySelector(`#${field}`)
+				self.validateFields(input)
+			})
+		})
+	}
+
+	validateOnEntry() {
+		let self = this
+		this.fields.forEach((field) => {
+			const input = document.querySelector(`#${field}`)
+
+			input.addEventListener('input', (event) => {
+				self.validateFields(input)
+			})
+		})
+	}
+
+	validateFields(field) {
+		// Check if fields are empty
+		if (field.value.trim() === '') {
+			this.setStatus(field, `${field.placeholder} cannot be blank`, 'error')
+		} else {
+			this.setStatus(field, null, 'success')
+			// Check for valid email
+			if (field.type === 'email') {
+				const re = /\S+@\S+\.\S+/
+				if (re.test(field.value)) {
+					this.setStatus(field, null, 'success')
+				} else {
+					this.setStatus(field, 'Looks like this is not an email', 'error')
+				}
+			}
+		}
+	}
+
+	setStatus(field, message, status) {
+		const errorIcon = field.parentElement.querySelector('.error-icon')
+		const errorMessage = field.parentElement.querySelector('.error-text')
+
+		if (status === 'success') {
+			if (errorIcon) {
+				errorIcon.classList.add('hidden')
+			}
+			if (errorMessage) {
+				errorMessage.innerText = ''
+			}
+			field.classList.remove('input-error')
+		}
+		if (status === 'error') {
+			field.parentElement.querySelector('.error-text').innerText = message
+			errorIcon.classList.remove('hidden')
+			field.classList.add('input-error')
+		}
 	}
 }
 
-//validate LastName
-function validateLastName() {
-	const lastName = $('#lname').val();
-	const $lastNameInput = $('#lname');
-	const $lNameErrorIcon = $('#lname-error-icon');
-	const $lNameErrorText = $('#lname-error-text');
+const form = document.querySelector('form')
+const fields = ['fname', 'lname', 'email', 'password']
 
-	if (lastName === '' || lastName == null) {
-		$lNameErrorIcon.css({ display: 'block' });
-		$lNameErrorText.css({ display: 'block' });
-		$lastNameInput.css({ border: '2px solid #ff7a7a' });
-	} else {
-		$lNameErrorIcon.css({ display: 'none' });
-		$lNameErrorText.css({ display: 'none' });
-		$lastNameInput.css({ border: '1px solid #dedede' });
-	}
-}
+const validator = new FormValidator(form, fields)
 
-//validate Email
-function validateEmail() {
-	const email = $('#email').val();
-	const $emailInput = $('#email');
-	const $emailErrorIcon = $('#email-error-icon');
-	const $emailErrorText = $('#email-error-text');
-
-	function checkEmail(email) {
-		const re = /\S+@\S+\.\S+/;
-		return re.test(email);
-	}
-
-	if (email === '' || email == null) {
-		$emailErrorIcon.css({ display: 'block' });
-		$emailErrorText.css({ display: 'block' });
-		$emailInput.css({ border: '2px solid #ff7a7a' });
-	} else if (!checkEmail(email)) {
-		$emailErrorIcon.css({ display: 'block' });
-		$emailErrorText
-			.text('Looks like this is not an email')
-			.css({ display: 'block' });
-		$emailInput.css({ border: '2px solid #ff7a7a', color: '#ff7a7a' });
-	} else {
-		$emailErrorIcon.css({ display: 'none' });
-		$emailErrorText.css({ display: 'none' });
-		$emailInput.css({ border: '1px solid #dedede', color: '#3e3c49' });
-	}
-}
-
-//validate Password
-function validatePassword() {
-	const password = $('#password').val();
-	const $passwordInput = $('#password');
-	const $passwordErrorIcon = $('#password-error-icon');
-	const $passwordErrorText = $('#password-error-text');
-
-	if (password === '' || password == null) {
-		$passwordErrorIcon.css({ display: 'block' });
-		$passwordErrorText.css({ display: 'block' });
-		$passwordInput.css({ border: '2px solid #ff7a7a' });
-	} else {
-		$passwordErrorIcon.css({ display: 'none' });
-		$passwordErrorText.css({ display: 'none' });
-		$passwordInput.css({ border: '1px solid #dedede' });
-	}
-}
-
-$('#submit').click(function (e) {
-	validateFirstName();
-	validateLastName();
-	validateEmail();
-	validatePassword();
-	e.preventDefault();
-});
+validator.initialize()
